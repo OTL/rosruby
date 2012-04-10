11ef77a8758e51f9b1557ebd536b38c1c018c865
Takashi Ogura <t.ogura@gmail.com>
2012-04-10 11:52:20 +0900

    use Std_msgs moduel for String.msg

--- test/test_publisher.rb
+++ test/test_publisher.rb
@@ -1,13 +1,13 @@
 #! /usr/bin/ruby 
 
 require 'ros/node'
-require 'ros/string'
+require 'std_msgs/string'
 
 def main
   node = ROS::Node.new('hoge')
-  publisher = node.advertise('/topic_test2', ROS::String)
+  publisher = node.advertise('/topic_test2', Std_msgs::String)
   sleep(1)
-  msg = ROS::String.new
+  msg = Std_msgs::String.new
   msg.data = 'hogehoge'
   while node.ok?
     publisher.publish(msg)
