8c99bbf8be7eece49db3963eb5d21e1a2f05cb09
Takashi Ogura <t.ogura@gmail.com>
2012-04-12 23:38:49 +0900

    fix some bugs

--- test/test_header.rb
+++ test/test_header.rb
@@ -8,7 +8,11 @@ class TestParam_Normal < Test::Unit::TestCase
     header = ROS::TCPROS::Header.new
     header['aa'] = '5'
     assert_equal('5', header['aa'])
-    assert_equal("\b\000\000\000\004\000\000\000aa=5", header.serialize)
+    sio = StringIO.new
+    header.serialize(sio)
+    sio.rewind
+    data = sio.read
+    assert_equal("\b\000\000\000\004\000\000\000aa=5", data)
 
     header2 = ROS::TCPROS::Header.new
     assert_equal(header2, header2.deserialize("\b\000\000\000\004\000\000\000aa=5"[4..-1]))
