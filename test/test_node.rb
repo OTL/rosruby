001fd4312656c2eb37b35723545b2a38a5c61bc1
Takashi Ogura <t.ogura@gmail.com>
2012-04-14 02:58:33 +0900

    support anonymous node (closes #14) - add test

--- test/test_node.rb
+++ test/test_node.rb
@@ -6,6 +6,7 @@ require 'test/unit'
 class TestNode < Test::Unit::TestCase
   def test_up_down
     node1 = ROS::Node.new('/test1')
+    assert_equal('/test1', node1.node_name)
     assert(node1.ok?)
     node1.shutdown
     assert(!node1.ok?)
@@ -23,6 +24,30 @@ class TestNode < Test::Unit::TestCase
     node1.shutdown
     node2.shutdown
     assert(!node1.ok?)
+    assert(!node2.ok?)
+  end
+
+  def test_anonymous
+    node1 = ROS::Node.new('/test1', true)
+    assert_not_equal('/test1', node1.node_name)
+    node2 = ROS::Node.new('/test1', true)
+    assert_not_equal('/test1', node2.node_name)
+    sleep(0.5)
+    assert(node1.ok?)
+    assert(node2.ok?)
+    node1.shutdown
+    node2.shutdown
+    assert(!node1.ok?)
+    assert(!node2.ok?)
+  end
+
+  def test_not_anonymous
+    node1 = ROS::Node.new('/test1')
+    node2 = ROS::Node.new('/test1')
+    sleep(0.5)
+    assert(!node1.ok?) # killed by master
+    assert(node2.ok?)
+    node2.shutdown
     assert(!node2.ok?)
   end
 
