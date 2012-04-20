2db129d75ed34e94a2654cbd9c66840a751c4b45
Takashi Ogura <t.ogura@gmail.com>
2012-04-20 11:22:43 +0900

    generate depends packages

--- scripts/gen_for_precompiled.py
+++ scripts/gen_for_precompiled.py
@@ -8,6 +8,7 @@
 import genmsg_ruby
 import gensrv_ruby
 from roslib.packages import get_pkg_dir
+from roslib.packages import ROSPackages
 
 import sys
 import os
@@ -18,15 +19,26 @@ if __name__ == "__main__":
         packages = ['std_msgs', 'rosgraph_msgs', 'roscpp_tutorials']
     else:
         packages = sys.argv[1:]
+    rp = ROSPackages()
+    depends = rp.depends(packages)
+    all_deps = set()
+    for packs in depends.values():
+        for pack in packs:
+            all_deps.add(pack)
     for pack in packages:
+        all_deps.add(pack)
+    print all_deps
+    for pack in all_deps:
         msg_dir = "%s/msg/"%get_pkg_dir(pack)
         msg_output_prefix = "%s/msg_gen/ruby"%get_pkg_dir('rosruby')
         if os.path.exists(msg_dir):
             for file in os.listdir(msg_dir):
+                print "generating " + file
                 genmsg_ruby.gen_msg(msg_dir+file, msg_output_prefix)
 
         srv_dir = "%s/srv/"%get_pkg_dir(pack)
         srv_output_prefix = "%s/srv_gen/ruby"%get_pkg_dir('rosruby')
         if os.path.exists(srv_dir):
             for file in os.listdir(srv_dir):
+                print "generating " + file
                 gensrv_ruby.gen_srv(srv_dir+file, srv_output_prefix)
