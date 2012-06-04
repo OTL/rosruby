7a33ab706c6084cd1eb8bff3625c3e88b9e67d88
Takashi Ogura <t.ogura@gmail.com>
2012-06-04 18:31:33 +0900

    Update tf.

--- rosruby_tf/lib/tf/listener.rb
+++ rosruby_tf/lib/tf/listener.rb
@@ -1,12 +1,29 @@
 require 'ros'
+require 'tf/transformer'
 require 'tf/tfMessage'
 require 'geometry_msgs/TransformStamped'
 
 module Tf
   class TransformListener
     def initialize(node)
-      @subscriber = node.subscribe("/tf", TfMessage) do |tf|
+      @transform_buffer = TransformBuffer.new
+      @subscriber = node.subscribe("/tf", TfMessage) do |tf_msg|
+        tf_msg.transforms.each do |tf|
+          parent = @transform_buffer.find_transform(tf.header.frame_id,
+                                                    tf.header.stamp)
 
+          transform = Transform.new(tf.child_frame,
+                                    [tf.transform.translation.x,
+                                     tf.transform.translation.y,
+                                     tf.transform.translation.z],
+                                    [tf.transform.rotation.x,
+                                     tf.transform.rotation.y,
+                                     tf.transform.rotation.z,
+                                     tf.transform.rotation.w],
+                                    parent)
+          tranform.stamp = tf.header.stamp
+          @transformer.add_transform(tranform)
+        end
       end
       @thread = Thread.new do
         while node.ok?
@@ -14,6 +31,12 @@ module Tf
           @subscriber.process_queue
         end
       end
+    end
+
+    def lookup_transform(from_id, to_id, stamp)
+      from = @transform_buffer.find_transform(from_id, stamp)
+      to = @transform_buffer.find_transform(to_id, stamp)
+      from.get_transform_to(to)
     end
 
     def shutdown
--- rosruby_tf/test/test_transformer.rb
+++ rosruby_tf/test/test_transformer.rb
@@ -16,9 +16,9 @@ class TestTransformer < Test::Unit::TestCase
     @root = Tf::Transform.new('/root', [0, 0, 0], [0, 0, 0, 1], nil)
     @frame1 = Tf::Transform.new('/frame1', [1, 0, 0], [0, 0, 0, 1], @root)
     @frame2 = Tf::Transform.new('/frame2', [1, 0, 0], [0, 0, 0, 1], @frame1)
-    @frame3 = Tf::Transform.new('/frame3', [1, 0, 0], [0, 0, 0, 1], @frame1)
-    @framea = Tf::Transform.new('/framea', [1, 0, 0], [0, 0, 0, 1], @root)
-    @frameb = Tf::Transform.new('/frameb', [1, 0, 0], [0, 0, 0, 1], @framea)
+    @frame3 = Tf::Transform.new('/frame3', [0, -1, 0], [0, 0, 0, 1], @frame1)
+    @framea = Tf::Transform.new('/framea', [-1, 0, 0], [0, 0, 0, 1], @root)
+    @frameb = Tf::Transform.new('/frameb', [1, 1, 0], [0, 0, 0, 1], @framea)
   end
 
   def test_root
@@ -34,4 +34,9 @@ class TestTransformer < Test::Unit::TestCase
     assert_equal([@frame2, @frame1, @frame3], @frame2.get_path(@frame3))
   end
 
+  def test_transform_chain
+    puts @framea.get_transform_to(@frame3)
+    puts @frame3.get_transform_to(@framea)
+    puts @root.get_transform_to(@frame3)
+  end
 end
