e194f5a00170ecb4abfe493142033ed7bd94a5fe
Takashi Ogura <t.ogura@gmail.com>
2013-10-05 13:19:40 +0900

    change script name because binary build fail (it can not be executed)

--- cmake/rosruby-macro.cmake
+++ cmake/rosruby-macro.cmake
@@ -18,7 +18,7 @@ endmacro()
 
 macro(rosruby_generate_messages)
   add_custom_target(rosruby_genmsg_for_${PROJECT_NAME} ALL
-    COMMAND ${ROSRUBY_GENMSG_DIR}/genrb_pkg ${ARGN}
+    COMMAND ${ROSRUBY_GENMSG_DIR}/genrb_pkg.sh ${ARGN}
     -d ${ROSRUBY_DEVEL_LIB_DESTINATION}
     )
   foreach(package ${ARGN})
