59040d7edff46911ddd0287e1db92993d1287aa1
Takashi Ogura <t.ogura@gmail.com>
2013-10-10 00:56:57 +0900

    remove shell script and use CATKIN_ENV

--- cmake/rosruby-macro.cmake
+++ cmake/rosruby-macro.cmake
@@ -18,7 +18,7 @@ endmacro()
 
 macro(rosruby_generate_messages)
   add_custom_target(rosruby_genmsg_for_${PROJECT_NAME} ALL
-    COMMAND ${ROSRUBY_GENMSG_DIR}/genrb_pkg.sh ${ARGN}
+    COMMAND ${CATKIN_ENV} ${ROSRUBY_GENMSG_DIR}/rosruby_genmsg.py ${ARGN}
     -d ${ROSRUBY_DEVEL_LIB_DESTINATION}
     )
   foreach(package ${ARGN})
--- scripts/genrb_pkg.sh
+++
@@ -1,2 +0,0 @@
-#/usr/bin/bash
-. /opt/ros/hydro/setup.sh && `dirname $0`/rosruby_genmsg.py $*
